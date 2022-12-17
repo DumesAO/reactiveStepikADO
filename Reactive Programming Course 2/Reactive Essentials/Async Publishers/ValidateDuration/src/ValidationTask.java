219d82e461f8e18a020392e688be688cf74c93e9
DumesAO <64592207+DumesAO@users.noreply.github.com>
2022-12-17 21:04:57 +0200

    5
    5

--- Reactive Programming Course 2/Reactive Essentials/Async Publishers/ValidateDuration/src/ValidationTask.java
+++ Reactive Programming Course 2/Reactive Essentials/Async Publishers/ValidateDuration/src/ValidationTask.java
@@ -1,10 +1,15 @@
 import java.time.Duration;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class ValidationTask {
 
-	public static boolean validate(Duration duration) {
-		return !duration.isNegative() && !duration.isZero();
+	public static Mono<Void> validate(Duration duration) {
+		if (duration.isNegative() || duration.isZero()) {
+			return Mono.error(new IllegalArgumentException("wrong duration"));
+		} else {
+			return Mono.empty();
+		}
 	}
 }
