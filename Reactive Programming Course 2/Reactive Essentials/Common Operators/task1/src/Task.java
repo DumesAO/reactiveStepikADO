7e10f60c214476c2541a1969d6abd36761446cd2
DumesAO <64592207+DumesAO@users.noreply.github.com>
2022-12-17 21:17:18 +0200

    7

--- Reactive Programming Course 2/Reactive Essentials/Common Operators/task1/src/Task.java
+++ Reactive Programming Course 2/Reactive Essentials/Common Operators/task1/src/Task.java
@@ -6,6 +6,6 @@ import reactor.core.publisher.Flux;
 public class Task {
 
 	public static <T> Publisher<String> transformSequence(Publisher<T> publisher) {
-		return Flux.error(new ToDoException());
+		return Flux.from(publisher).map(Object::toString);
 	}
 }
