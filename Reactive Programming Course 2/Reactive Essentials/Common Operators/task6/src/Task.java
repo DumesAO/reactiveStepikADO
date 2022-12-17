a8cd2560902c4e0411c55f83618412b92305c194
DumesAO <64592207+DumesAO@users.noreply.github.com>
2022-12-17 21:37:00 +0200

    12

--- Reactive Programming Course 2/Reactive Essentials/Common Operators/task6/src/Task.java
+++ Reactive Programming Course 2/Reactive Essentials/Common Operators/task6/src/Task.java
@@ -4,6 +4,6 @@ import reactor.core.publisher.Mono;
 public class Task {
 
 	public static Mono<Long> createSequence(Flux<Long> flux) {
-		return Mono.error(new ToDoException());
+		return flux.reduce(Long::sum);
 	}
 }
